a29e2d5098d3b012229066ac8f0f1cfdf3f14045
SnowSword1010 <jmayank2001@gmail.com>
2024-07-29 01:38:58 +0530

    :sparkles: Working code

--- samba-client-1/app/app.py
+++ samba-client-1/app/app.py
@@ -9,52 +9,38 @@ SHARE_NAME = 'samba_share'
 username = ''
 password = ''
 
-conn = SMBConnection(username, password, "client_machine", SAMBA_SERVER, use_ntlm_v2=True)
-assert conn.connect(SAMBA_SERVER, 139)
 
 @app.route('/')
 def index():
+    conn = SMBConnection(username, password, "client_machine", SAMBA_SERVER, use_ntlm_v2=True)
+    assert conn.connect(SAMBA_SERVER, 139)
+
     files = conn.listPath(SHARE_NAME, '/')
+    files = [file for file in files if not file.isDirectory]
 
     file_name_list = [file.filename for file in files]
+
+    conn.close()
 
     return render_template('index.html', files=file_name_list)
 
 @app.route('/view/<filename>')
 def view_file(filename):
+    conn = SMBConnection(username, password, "client_machine", SAMBA_SERVER, use_ntlm_v2=True)
+    assert conn.connect(SAMBA_SERVER, 139)
+
     file_obj = io.BytesIO()
     conn.retrieveFile(SHARE_NAME, filename, file_obj)
     file_obj.seek(0)
 
-    # Determine content type based on file extension
+    conn.close()
+
     if filename.endswith('.txt'):
         return send_file(file_obj, mimetype='text/plain', as_attachment=False)
     elif filename.endswith(('.png', '.jpg', '.jpeg', '.gif')):
         return send_file(file_obj, mimetype='image/jpeg', as_attachment=False)
     else:
         return Response("Unsupported file type", mimetype='text/plain')
-    
-
-@app.teardown_appcontext
-def close_connection(exception):
-    if conn:
-        conn.close()
-
-
-    # file_path = f'//{SAMBA_SERVER}/{SHARE_NAME}/{filename}'
-    # file_content = os.popen(f'smbclient {file_path} -N -c "get {filename} -"').read()
-    
-    # if '.txt' in filename:
-    #     return Response(file_content, mimetype='text/plain')
-    
-    # elif '.jpg' in filename or '.jpeg' in filename:
-    #     return Response(file_content, mimetype='image/jpeg')
-    
-    # elif '.png' in filename:
-    #     return Response(file_content, mimetype='image/png')
-    
-    # else:
-    #     return Response("Unsupported file type", mimetype='text/plain')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
